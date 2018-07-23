3b97c9beca0348545f7199701dce773c901006ea
crdbyb <crdbyb@example.com>
2018-07-23 09:39:47 +0800

    田正东

---
+++ Code/田正东/1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#define N 10
+int main()
+{
+    int a[N][3];
+    int i=0;
+    int j=0;
+    //memset(a,0,sizeof(a));
+       while(scanf("%d %d",&a[i][j],&a[i][j+1])!=EOF){
+        a[i][j+2]=a[i][j]+a[i][j+1];
+        i++;
+       }
+   for(j=0;j<i;j++)
+   {
+       printf("%d\n",a[j][2]);
+   }
+    return 0;
+}
